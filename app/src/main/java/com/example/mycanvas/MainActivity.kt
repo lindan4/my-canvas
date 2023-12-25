bde094fcf89be886c8701395cce37162cf4c6f95
Lindan Thillanayagam <lindthilldev@gmail.com>
2023-12-24 19:36:09 -0500

    Created brush buttons.

--- app/src/main/java/com/example/mycanvas/MainActivity.kt
+++ app/src/main/java/com/example/mycanvas/MainActivity.kt
@@ -7,13 +7,16 @@ class MainActivity : ComponentActivity() {
 
     private var drawingView: DrawingActivity? = null
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawingView = findViewById(R.id.a_draw_view)
         drawingView?.setSizeForBrush(20.toFloat())
+    }
+
+    private fun showBrushSize() {
+
+
     }
 }
