bd5651d3428be3262e78a5b8b73e684588c95531
Lindan Thillanayagam <lindthilldev@gmail.com>
2023-12-25 17:55:59 -0500

    Can toggle brush sizes.

--- app/src/main/java/com/example/mycanvas/MainActivity.kt
+++ app/src/main/java/com/example/mycanvas/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mycanvas
 
+import android.app.Dialog
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
     private var drawingView: DrawingActivity? = null
+
+    private var brushSelectionButton: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,10 +18,38 @@ class MainActivity : ComponentActivity() {
 
         drawingView = findViewById(R.id.a_draw_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        brushSelectionButton = findViewById(R.id.brush_selection)
+
+        brushSelectionButton?.setOnClickListener {
+            showBrushSizeDialog()
+        }
     }
 
-    private fun showBrushSize() {
+    private fun showBrushSizeDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Select Brush Size")
 
+        val smallButton: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        val mediumButton: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        val largeButton: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
 
+        smallButton.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        mediumButton.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        largeButton.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
